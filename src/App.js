8ebd5e2b05f403de132154fa09925087cb8d1026
Vijay Guttula <jarvis@jarvis.local>
2021-11-29 19:47:33 +0530

    about me init, font awe icons, react router dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
 import Header from './components/Header/Header';
+import AboutMe from './components/About Me/AboutMe';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <>
       <Header />
+      <div className='container'></div>
+      <Router>
+        <Switch>
+          <Route path='/' component={AboutMe}>
+            <AboutMe />
+          </Route>
+        </Switch>
+      </Router>
     </>
   );
 }
