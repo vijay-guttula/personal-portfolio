464203d3b89b941bcecdce462103ec59417d0501
Vijay Guttula <ssvijayg@gmail.com>
2021-12-02 00:15:03 +0530

    css tweeked

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -3,11 +3,14 @@ import React from 'react';
 const Contact = () => {
   return (
     <div>
-      <div className='title'>Let's Chat</div>
-      <div className='subTitle'>
+      <div className='title text-3xl'>Let's Chat</div>
+      <div className='subTitle text-lg mt-3'>
         I'd love to hear about your cool ideas and work together.
       </div>
-      <div className='contacts mt-3' style={{ fontSize: '0.85rem' }}>
+      <div
+        className='contacts mt-3 tracking-wide leading-relaxed'
+        style={{ fontSize: '0.85rem' }}
+      >
         <div className='contact fa-lg no-underline hover:underline'>
           <a href='mailto:ssvijayg@gmail.com'>
             <i className='far fa-paper-plane mr-1'></i>Mail
--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -3,10 +3,18 @@ import React from 'react';
 const Intro = () => {
   return (
     <div>
-      <div className='introHeader'>Hello,</div>
-      <div className='introDes'>
-        I'm Vijay Guttula, a senior CS undergrad, currently working as an SDE
-        Intern @<a href='www.gotoolbox.in'>Toolbox</a>
+      <div className='introHeader inline-block text-6xl pl-1'>Hello,</div>
+      <div className='introDes text-xl'>
+        I'm<span className='font-bold'> Vijay Guttula</span>, a
+        <span className='font-bold'> Senior CS Undergrad</span>, currently
+        working as an <span> </span>
+        <span className='font-bold'>
+          SDE Intern @
+          <a className='no-underline hover:underline' href='www.gotoolbox.in'>
+            Toolbox
+          </a>
+        </span>
+        .
       </div>
     </div>
   );
