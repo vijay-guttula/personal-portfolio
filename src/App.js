d5b235d3c3efba6bbfa4d9e2e732189699b737e3
Vijay Guttula <jarvis@jarvis.local>
2021-12-01 22:32:59 +0530

    tailwind init and projects done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import './App.css';
+import Intro from './components/Intro/Intro';
+import Projects from './components/Projects/Projects';
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className='somethingIhavepickedupoftheInternet'>&nbsp;</div>
+      <div className='md:container md:mx-auto subpixel-antialiased Container'>
+        <Intro />
+      </div>
+      <div className='md:container md:mx-auto subpixel-antialiased Container'>
+        <Projects />
+      </div>
+    </>
+  );
 }
 
 export default App;
