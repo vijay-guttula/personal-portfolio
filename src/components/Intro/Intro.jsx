eddd429693588e5ca3fd4dd3edb479eca76ec718
Vijay Guttula <ssvijayg@gmail.com>
2021-12-01 22:32:59 +0530

    tailwind init and projects done

---
+++ src/components/Intro/Intro.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Intro = () => {
+  return (
+    <div>
+      <div className='introHeader'>Hello,</div>
+      <div className='introDes'>
+        I'm Vijay Guttula, a senior CS undergrad, currently working as an SDE
+        Intern @<a href='www.gotoolbox.in'>Toolbox</a>
+      </div>
+    </div>
+  );
+};
+
+export default Intro;
