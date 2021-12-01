6d4049831af3c45188aa57fa704b4d041d86bb25
Vijay Guttula <jarvis@jarvis.local>
2021-12-01 20:41:46 +0530

    scurrent build deffered

--- src/App.js
+++ src/App.js
@@ -1,26 +1,7 @@
 import './App.css';
-import Header from './components/Header/Header';
-import AboutMe from './components/About Me/AboutMe';
-import Resume from './components/Resume/Resume';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
-  return (
-    <>
-      <Router>
-        <Header />
-        <div className='Container'></div>
-        <Switch>
-          <Route exact path='/' component={AboutMe}>
-            <AboutMe />
-          </Route>
-          <Route path='/resume' component={Resume}>
-            <Resume />
-          </Route>
-        </Switch>
-      </Router>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
