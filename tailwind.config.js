be8c39861a6ef6fcbfbcf6cdd0f2673febb7d05f
Vijay Guttula <ssvijayg@gmail.com>
2021-12-02 01:18:01 +0530

    css responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,10 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    screens: {
+      mobile: '360px',
+      md: '720px',
+    },
   },
   variants: {
     extend: {},
