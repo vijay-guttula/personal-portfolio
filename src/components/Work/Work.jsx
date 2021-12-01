38dc112cbb79ea1bc0cd1526076262bae88d712c
Vijay Guttula <ssvijayg@gmail.com>
2021-12-02 00:25:47 +0530

    css tweeked

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -5,12 +5,18 @@ const Work = () => {
     <div>
       <div className='title text-3xl'>Work</div>
       <div className='subTitle text-xl mt-3'>
-        Working as an SDE intern @Toolbox since Aug, 2020.
+        Working as an<span> </span>
+        <span className='font-bold'>
+          SDE Intern @
+          <a className='no-underline hover:underline' href='www.gotoolbox.in'>
+            Toolbox
+          </a>
+        </span>
+        <span> </span>
+        since <span className='font-bold'>Aug, 2020</span>.
       </div>
-      <div className='subTitle text-lg mt-2'>
-        Things I've done in the internship
-      </div>
-      <ul className='list-disc text-lg mt-1 ml-5 tracking-wide leading-relaxed'>
+      <div className='subTitle text-lg'>Things I've done in the internship</div>
+      <ul className='list-disc text-lg ml-5 tracking-wide leading-relaxed'>
         <li>Integrated the cancel order flow to the store</li>
         <li>Added module to support banners for targetted promotions</li>
         <li>Integrated the product video upload to dashboard</li>
