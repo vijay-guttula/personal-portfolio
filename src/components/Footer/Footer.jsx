76c3520cdf8b7267ff1470e3528d288a9263f2dc
Vijay Guttula <jarvis@jarvis.local>
2021-12-04 16:09:00 +0530

    footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div className='mt-6 text-sm self-center justify-self-center text-center text-gray-400 mb-4'>
+      Made using Gatsby &amp; TailwindCss.
+      <a
+        className='text-black'
+        href='https://github.com/vijay-guttula/personal-portfolio'
+      >
+        {' '}
+        Source
+      </a>
+    </div>
+  );
+};
+
+export default Footer;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: false,
   theme: {
+    screens: {
+      mobile: '360px',
+      md: '720px',
+    },
     extend: {},
   },
   variants: {
