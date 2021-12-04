d9aeb994048504a674abad94cf1961fe9646541f
Vijay Guttula <ssvijayg@gmail.com>
2021-12-04 19:16:30 +0530

    SEO added

---
+++ src/components/SEO/SEO.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Helmet } from 'react-helmet';
+import { useLocation } from '@reach/router';
+import { useStaticQuery, graphql } from 'gatsby';
+
+const Seo = ({ title }) => {
+  const { pathname } = useLocation();
+  const { site } = useStaticQuery(query);
+
+  const { defaultTitle, siteUrl, twitterUsername } = site.siteMetadata;
+
+  const seo = {
+    title: title || defaultTitle,
+    url: `${siteUrl}${pathname}`,
+  };
+
+  return (
+    <Helmet title={seo.title}>
+      {seo.url && <meta property='og:url' content={seo.url} />}
+
+      {seo.title && <meta property='og:title' content={seo.title} />}
+
+      {twitterUsername && (
+        <meta name='twitter:creator' content={twitterUsername} />
+      )}
+    </Helmet>
+  );
+};
+
+export default Seo;
+
+Seo.propTypes = {
+  title: PropTypes.string,
+};
+
+Seo.defaultProps = {
+  title: null,
+};
+
+const query = graphql`
+  query Seo {
+    site {
+      siteMetadata {
+        defaultTitle: title
+        siteUrl: url
+        twitterUsername
+      }
+    }
+  }
+`;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,15 @@
 import * as React from 'react';
 import App from './App';
+import Seo from '../components/SEO/SEO';
 
 // markup
 const IndexPage = () => {
-  return <App />;
+  return (
+    <>
+      <Seo />
+      <App />
+    </>
+  );
 };
 
 export default IndexPage;
