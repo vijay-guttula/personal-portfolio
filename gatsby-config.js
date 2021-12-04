287d983494cbece1d9f2a395d9686962fe8b1a3e
Vijay Guttula <ssvijayg@gmail.com>
2021-12-04 19:00:41 +0530

    helmet package for site metadata used

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,15 @@
 module.exports = {
   siteMetadata: {
     siteUrl: 'https://www.vijayguttula.com',
-    title: 'personal portfolio',
+    title: 'Vijay Guttula',
+    description:
+      'Hogwarts Potions master, Head of Slytherin house and former Death Eater.',
+    url: 'https://www.vijayguttula.com', // No trailing slash allowed!
+    twitterUsername: '@vijayguttula',
   },
-  plugins: ['gatsby-plugin-postcss', 'gatsby-plugin-fontawesome-css'],
+  plugins: [
+    'gatsby-plugin-postcss',
+    'gatsby-plugin-fontawesome-css',
+    'gatsby-plugin-react-helmet',
+  ],
 };
