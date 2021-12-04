2ad062278e608979234759a453313175cd301b64
Vijay Guttula <jarvis@jarvis.local>
2021-12-04 13:40:15 +0530

    project backedup

---
+++ backups/v1/src/components/Contact/Contact.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div>
+      <div className='title text-3xl'>Let's Chat</div>
+      <div className='subTitle text-lg mt-3'>
+        I'd love to hear about your cool ideas and work together.
+      </div>
+      <div
+        className='contacts mt-3 tracking-wide leading-relaxed'
+        style={{ fontSize: '0.85rem' }}
+      >
+        <div className='contact fa-lg no-underline hover:underline'>
+          <a href='mailto:ssvijayg@gmail.com'>
+            <i className='far fa-paper-plane mr-1'></i>Mail
+          </a>
+        </div>
+        <div className='contact mt-3 fa-lg no-underline hover:underline'>
+          <a href='https://github.com/vijay-guttula'>
+            <i className='fab fa-github mr-1'></i>Github
+          </a>
+        </div>
+        <div className='contact mt-3 fa-lg no-underline hover:underline'>
+          <a href='https://twitter.com/vj_guttula'>
+            <i className='fab fa-twitter mr-1'></i>Twitter
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
