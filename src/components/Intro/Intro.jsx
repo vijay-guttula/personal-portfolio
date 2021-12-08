6c3084b131befc003adb6e9dbfb2d9b52119ea82
Vijay Guttula <ssvijayg@gmail.com>
2021-12-08 07:29:03 +0530

    padding

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Intro = () => {
   return (
-    <div>
+    <div className='pt-5'>
       <div className='introHeader inline-block pl-1  text-6xl'>Hello,</div>
-      <div className='introDes text-xl inline-block mt-3'>
+      <div className='introDes text-xl inline-block mt-2 pt-3'>
         I'm<span className='font-bold'> Vijay Guttula</span>, a
         <span className='font-bold'> Senior CS Undergrad</span>, currently
         working as an <span> </span>
